e91675f9287bf76616cc4f19f1d3ec21e2282540
xbugliu <532469278@qq.com>
2022-04-13 20:20:27 +0800

    test code

--- src/lib/tantivy_jieba.rs
+++ src/lib/tantivy_jieba.rs
@@ -93,7 +93,7 @@ mod tests {
     #[test]
     fn it_works() {
         use tantivy::tokenizer::*;
-        let tokenizer = crate::JiebaTokenizer {};
+        let tokenizer = crate::tantivy_jieba::JiebaTokenizer {};
         let mut token_stream = tokenizer.token_stream(
             "张华考上了北京大学；李萍进了中等技术学校；我在百货公司当售货员：我们都有光明的前途",
         );
